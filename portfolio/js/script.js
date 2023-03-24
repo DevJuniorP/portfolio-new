3e590ec688c358a7d960c533403f3a96337275a5
GilmarPeron <109175794+DevJuniorP@users.noreply.github.com>
2023-03-24 06:49:26 -0400

    Att Portfolio
    24032023

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,5 +1,12 @@
 let menuToggle = document.querySelector(".menuToggle");
 let header = document.querySelector("header");
+let typedEffect = new Typed(".typed", {
+    strings : ["Junior Peron", "Dev Front-End"],
+    loop : true,
+    typedSpeed : 100,
+    backSpeed : 50,
+    backDelay: 100,
+})
 
 menuToggle.onclick = function () {
     header.classList.toggle('active');
@@ -9,3 +16,4 @@ window.sr = ScrollReveal({ reset: true});
 
 sr.reveal('#sobre', { duration: 2000});
 sr.reveal('#portfolio', { duration: 2000});
+sr.reveal('#contato', { duration: 2000});
